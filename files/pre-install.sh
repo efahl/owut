286d10141bc3698fdcc700097de1a615623e58de
Eric Fahlgren <ericfahlgren@gmail.com>
2024-11-29 09:11:14 -0800

    examples: update the pre-install.sh with firmware backup
    Fix some typos and add firmware backup to the remote backup example.
    
    Signed-off-by: Eric Fahlgren <ericfahlgren@gmail.com>

--- files/pre-install.sh
+++ files/pre-install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Example pre-installation hook
 #
+# To see the most recent version of this file, go to:
+#   https://github.com/efahl/owut/blob/main/files/pre-install.sh
+#
 # Allows the user to inject actions between the download/verify phases of an
 # upgrade and the installation step.  If the script fails, that is returns an
 # exit code != 0, then 'owut' will abort the install process.
@@ -8,8 +11,9 @@
 # You tell 'owut' to use the script by saying
 #   owut upgrade --pre-install /etc/owut.d/pre-install.sh
 #
-# To use it by default, without explicit an '--pre-install' on the command
-# line, add it to /etc/config/attendedsysupgrade:
+# To use it by default, without an explicit '--pre-install' on the command
+# line, add it to /etc/config/attendedsysupgrade (typically, you only need
+# to uncomment the line that's already there):
 #
 # config owut 'owut'
 #	option pre_install '/etc/owut.d/pre-install.sh'
@@ -19,7 +23,7 @@
 # Since /etc/owut.d/ is part of the default backup list, we can just copy the
 # manifest produced by the ASU server from /tmp to this directory.  We add a
 # time stamp to the name for convenience.
-if true; then
+if false; then
 	stamp="$(date +'%FT%H%M')"
 
 	archive="/etc/owut.d/firmware-manifest-${stamp}.json"
@@ -42,18 +46,22 @@ if false; then
 fi
 
 
-# Example 3 - remote auto-backup
+# Example 3 - remote auto-backup of config and new firmware
 # Use a remote machine to which you have ssh access as a backup host.
 if false; then
-	remote="my-nas:/public/backups/"
+	remote="my-nas:/public/backups"
 
 	stamp="$(date +'%FT%H%M')"
 	router=$(uci get system.@system[0].hostname)
 	backup="backup-${router}-${stamp}.tgz"
+	firmware="firmware-${router}-${stamp}.bin"
 
 	sysupgrade --create-backup "/tmp/$backup" || exit 1
-	scp "/tmp/$backup" "$remote" || exit 1
+	scp "/tmp/$backup" "${remote}/${backup}" || exit 1
 	echo "Created remote backup ${remote}/${backup}"
+
+	scp "/tmp/firmware.bin" "${remote}/${firmware}" || exit 1
+	echo "Copied image to ${remote}/${firmware}"
 fi
 
 
